28bc19d68d71599b8aec8c3eb075305d2b909af8
Bruna Campos <92brunacampos@gmail.com>
2022-05-15 18:46:38 -0300

    first steps

---
+++ building applications and systems/Overview/HelloWorld.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+
+	public static void main (String entrada[]) {
+	
+		int inteiro = 29;
+		double real = 1.69;
+		String frase = "Bruna Campos ";
+		boolean VF = true;	
+
+		if (VF == true) {
+			System.out.println("Eu sou a "  + frase + "tenho " + inteiro + " anos e tenho " + real + "m de altura" );
+		}
+		
+		System.exit(0);
+
+	}
+
+}
