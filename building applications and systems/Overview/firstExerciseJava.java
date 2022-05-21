3583f9b4bcaaaf7914889867fc08ab6393d84678
Bruna Campos <92brunacampos@gmail.com>
2022-05-21 16:32:07 -0300

    add first exercise

---
+++ building applications and systems/Overview/firstExerciseJava.java
@@ -0,0 +1,21 @@
+class firstExerciseJava {
+
+	public static void main (String entrada[]) {
+
+		String name, course;
+		char gender;
+		int age;
+		double weight;
+
+		name = "Bruna Campos";
+		course = "Ciencia da Computação";
+		gender = 'F';
+		age = 29;
+		weight = 70;
+
+		System.out.println("Me chamo " + name + ", me identifico com o genero " + gender + ", tenho " + age + " anos e peso " + weight + "kg. Faço faculdade de " + course + ".");
+		System.exit(0);
+
+	}
+
+}
